1f863f7367404c6d2db7b623ef9c36d878b87499
dannycarmen <danny-carmen@live.com>
2021-09-09 15:22:57 -0600

    Initial Commit

---
+++ src/components/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
+
+export default function () {
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="right-column">Placeholder Text</div>
+    </div>
+  );
+}
